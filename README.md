14ea3388b1128d41482c69f5dba094fe864b2e2d
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2022-03-14 18:18:58 +0700

    feat: init price monitoring

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# price-monitoring
+
+# Price monitoring
+
+A mini tool that tracks the changing price of products, helping you buy the product you want at the most reasonable price
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/hanknguyen14/price-monitoring
+```
+
+Go to the project directory
+
+```bash
+  cd price-monitoring
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Copy env example
+
+```bash
+  cp .env.example .env
+```
+
+Update env according to your parameters 
+
+```bash
+  PRODUCTS_URL = '/product-url'
+  EXPECTED_PRICE = 123456
+  REGEX_PRICE = "[^0-9,-]+"
+  PRICE_SELECTOR = '.class .price'
+  NAME_SELECTOR = '.name-product h1'
+  # Channel id from https://notify.run/
+  NOTIFICATION_CHANNEL_ID='notification_channel_id'
+  # Time in miliseconds
+  INTERVAL_TIME_DURATION=12345
+```
+
+Run start application
+```bash
+  npm start
+```
+
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+## Feedback
+
+If you have any feedback, please reach out to us at hungnguyen.dhg@gmail.com
+
---
+++ index.js
@@ -0,0 +1,54 @@
+import { load } from 'cheerio';
+import fetch from 'node-fetch';
+import 'dotenv/config';
+
+const PRODUCTS_URL = process.env.PRODUCTS_URL;
+const EXPECTED_PRICE = process.env.EXPECTED_PRICE;
+const REGEX_PRICE = new RegExp(process.env.REGEX_PRICE, 'g');
+const PRICE_SELECTOR = process.env.PRICE_SELECTOR;
+const NAME_SELECTOR = process.env.NAME_SELECTOR;
+const NOTIFICATION_CHANNEL_ID = process.env.NOTIFICATION_CHANNEL_ID;
+const INTERVAL_TIME_DURATION = process.env.INTERVAL_TIME_DURATION;
+
+const convertPrice = (amount) => parseInt(Number(amount.replace(REGEX_PRICE, "")));
+
+const getHTML = async (url) => {
+    const header = {
+        method: 'GET',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+        }
+    }
+    const res = await fetch(url, header);
+    const html = await res.text();
+    return load(html);
+}
+
+const sendPush = async (name, price) => {
+    const dataMessage = `Price went down: ${name} went below ${price}`;
+    const header = {
+        method: 'POST',
+        body: dataMessage,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+        }
+    }
+    return fetch(`https://notify.run/${NOTIFICATION_CHANNEL_ID}`, header);
+}
+
+const checkPrices = async (url, expectedPrice) => {
+    const $_parsedHTML = await getHTML(url);
+    const price = $_parsedHTML(PRICE_SELECTOR).text();
+    const name = $_parsedHTML(NAME_SELECTOR).text();
+    const priceInt = convertPrice(price);
+    if (priceInt <= expectedPrice) {
+        sendPush(name, priceInt);
+        console.log('Yay...Price went down', name, price);
+    } else {
+        console.log('No hope this time', name, price);
+    }
+}
+
+// Run the checkPrices function every INTERVAL_TIME_DURATION
+checkPrices(PRODUCTS_URL, EXPECTED_PRICE);
+setInterval(checkPrices, INTERVAL_TIME_DURATION, PRODUCTS_URL, EXPECTED_PRICE);
